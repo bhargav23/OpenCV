6261897e2be642b8161a61a7654dcf0043fec3c5
bhargav23 <bhargav.kiran@gmail.com>
2021-07-16 14:17:58 +0530

    Update and rename rname1.py to rename1.py

--- rename1.py
+++ rename1.py
@@ -6,21 +6,21 @@ import os
   
 # Function to rename multiple files
 def main():
-    path = '/content/drive/MyDrive/Data/BirdData/bgimages/'
+    path = '/content/drive/MyDrive/Data/BirdData/foreground/'
     t = 1;
     for count, filename in enumerate(os.listdir(path)):
-      if(count<9):
-        dst ="bg000" + str(t) + ".jpg"
-      elif(count<99):
-        dst ="bg00" + str(t) + ".jpg"
-      elif(count<999):
-        dst ="bg0" + str(t) + ".jpg"
+      if(t<10):
+        dst ="image00" + str(t) + ".png"
+      elif(t<100):
+          dst ="image0" + str(t) + ".png"
+      else: 
+            dst ="image" + str(t) + ".png"
       src = path + filename
       dst =path+ dst
       # rename() function will
       # rename all the files
       os.rename(src, dst)
-      t = t +1
+      t = t + 1
   
 # Driver Code
 if __name__ == '__main__':
