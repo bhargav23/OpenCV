ea70f3815b2b7b07bb704d407fa34ca436de72b7
bhargav23 <bhargav.kiran@gmail.com>
2021-07-23 00:50:06 +0530

    Update rename1.py

--- rename1.py
+++ rename1.py
@@ -6,15 +6,21 @@ import os
   
 # Function to rename multiple files
 def main():
-    path = '/content/drive/MyDrive/Data/BirdData/foreground/'
-    t = 1;
+    path = '/content/drive/MyDrive/Data/dowloads/'
+    t = 600;
     for count, filename in enumerate(os.listdir(path)):
-      if(t<10):
-        dst ="image00" + str(t) + ".png"
-      elif(t<100):
-          dst ="image0" + str(t) + ".png"
-      else: 
-            dst ="image" + str(t) + ".png"
+      if t+1<10:
+          dst ="image00000" + str(t+1) + ".jpg"
+      elif t+1<100:
+          dst ="image0000" + str(t+1) + ".jpg"
+      elif t+1<1000:
+          dst ="image000" + str(t+1) + ".jpg"
+      elif t+1<10000:
+          dst ="image00" + str(t+1) + ".jpg"
+      elif t+1<100000:
+          dst ="image0" + str(t+1) + ".jpg"
+      else:
+          dst ="image" + str(t+1) + ".jpg"
       src = path + filename
       dst =path+ dst
       # rename() function will
